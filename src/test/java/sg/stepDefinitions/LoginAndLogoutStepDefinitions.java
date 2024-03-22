b31456ea74b4ac6b4abfeafd2909d3d1b72ece71
Test_Amann <bhanuprakash23bsk@gmail.com>
2024-03-22 12:46:06 +0530

    Draft1

---
+++ src/test/java/sg/stepDefinitions/LoginAndLogoutStepDefinitions.java
@@ -0,0 +1,33 @@
+package sg.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginAndLogoutStepDefinitions {
+    @Given("User navigates the URL")
+    public void userNavigatesTheURL() {
+        
+    }
+
+    @Then("User perform login with valid credentials")
+    public void userPerformLoginWithValidCredentials() {
+        
+    }
+
+    @And("User verify login is successful")
+    public void userVerifyLoginIsSuccessful() {
+        
+    }
+
+    @When("User verify That logout link is display")
+    public void userVerifyThatLogoutLinkIsDisplay() {
+        
+    }
+
+
+    @Then("User logouts from the application")
+    public void userLogoutsFromTheApplication() {
+    }
+}
